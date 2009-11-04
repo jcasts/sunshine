7fb04b7a9b59129b451415c3e9f276b8bba868f7
jcastagna <jcastagna@fa7227d9-076c-0410-bae7-df9668f59714>
2009-11-04 01:38:02 +0000

    more tests
    git-svn-id: svn://subversion.flight.yellowpages.com/webtools/gems/sunshine/trunk@425 fa7227d9-076c-0410-bae7-df9668f59714

--- test/unit/test_deploy_server.rb
+++ test/unit/test_deploy_server.rb
@@ -5,6 +5,7 @@ class TestDeployServer < Test::Unit::TestCase
   def setup
     @app = Sunshine::App.new TEST_APP_CONFIG_FILE
     @deploy_server = Sunshine::DeployServer.new("nextgen@np4.wc1.yellowpages.com", @app)
+    @deploy_server.connect
   end
 
   def teardown
@@ -12,29 +13,31 @@ class TestDeployServer < Test::Unit::TestCase
   end
 
   def test_connect
-    @deploy_server.connect
     assert @deploy_server.connected?
   end
 
   def test_disconnect
-    @deploy_server.connect
     @deploy_server.disconnect
     assert !@deploy_server.connected?
   end
 
   def test_run
-    @deploy_server.connect
-    assert_equal "test", @deploy_server.run("echo 'test'").strip
+    assert_equal "test\n", @deploy_server.run("echo 'test'")
   end
 
   def test_run_with_block
+    i = 0
+    @deploy_server.run("echo 'line1'; echo 'line2'") do |stream, data|
+      i = i.next
+      assert_equal :stdout, stream
+      assert_equal "line#{i}\n", data
+    end
   end
 
   def test_run_with_stderr
   end
 
   def test_os_name
-    @deploy_server.connect
     assert_equal "linux", @deploy_server.os_name
   end
 
