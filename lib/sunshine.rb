2270bc15af06501c94a5ab8113179754a548bbf9
jcastagna <jcastagna@fa7227d9-076c-0410-bae7-df9668f59714>
2009-11-19 02:01:09 +0000

    added -l and -e cmd options to set respectively debug levels and deploy environments
    git-svn-id: svn://subversion.flight.yellowpages.com/webtools/gems/sunshine/trunk@499 fa7227d9-076c-0410-bae7-df9668f59714

--- lib/sunshine.rb
+++ lib/sunshine.rb
@@ -7,9 +7,12 @@ require 'net/scp'
 require 'erb'
 require 'logger'
 require 'rainbow'
+require 'optparse'
 
 
 module Sunshine
+
+  VERSION = '0.0.1'
 
   class CmdError < Exception; end
 
@@ -44,25 +47,60 @@ module Sunshine
   require "sunshine/servers/rainbows"
 
 
-  class << self
 
-    include Open3
+  def self.logger
+    @logger ||= Sunshine::Output.new :level => @options['level']
+  end
 
-    def logger
-      @logger ||= Sunshine::Output.new
+  def self.deploy_env
+    @options['deploy_env']
+  end
+
+  def self.run_local(str)
+    stdin, stdout, stderr = Open3.popen3(str)
+    stderr = stderr.read
+    raise(CmdError, "#{stderr}  when attempting to run '#{str}'") unless stderr.empty?
+    stdout.read.strip
+  end
+
+  def self.parse_args argv
+    options = {
+      'level' => :info,
+      'deploy_env' => :qa,
+    }
+
+    opts = OptionParser.new do |opt|
+      opt.program_name = File.basename $0
+      opt.version = Sunshine::VERSION
+      opt.release = nil
+      opt.banner = <<-EOF
+Usage: #{opt.program_name} [options]
+
+Sunshine is a gem that provides a light, consistant api for application deployment. 
+      EOF
+
+      opt.separator nil
+
+      opt.on('-l', '--level [LEVEL]',
+             'Set trace level. Defaults to info.') do |value|
+        options['level'] = value.downcase.to_sym
+      end
+
+      opt.on('-e', '--env [DEPLOY_ENV]',
+             'Sets the deploy environment. Defaults to qa.') do |value|
+        options['deploy_env'] = value
+      end
     end
 
-    def deploy_env
-      :qa
-    end
+    opts.parse! argv
 
-    def run_local(str)
-      stdin, stdout, stderr = popen3(str)
-      stderr = stderr.read
-      raise(CmdError, "#{stderr}  when attempting to run '#{str}'") unless stderr.empty?
-      stdout.read.strip
-    end
+    options
+  end
 
+  def self.run argv = ARGV
+    @options = parse_args argv
   end
 
 end
+
+Sunshine.run # Remove this once the gem is called from the cmd line
