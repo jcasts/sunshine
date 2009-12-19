94838bc478255a90d0a5542aeed3c5607f79ab4c
jcastagna <jcastagna@fa7227d9-076c-0410-bae7-df9668f59714>
2009-12-19 01:01:53 +0000

    updated rainbows config to support concurrency
    git-svn-id: svn://subversion.flight.yellowpages.com/webtools/gems/sunshine/trunk@671 fa7227d9-076c-0410-bae7-df9668f59714

--- lib/sunshine/servers/rainbows.rb
+++ lib/sunshine/servers/rainbows.rb
@@ -2,6 +2,20 @@ module Sunshine
 
   class Rainbows < Unicorn
 
+    attr_reader :concurrency
+
+    ##
+    # Assign and/or use a concurrency model. Supports all Rainbows concurrency
+    # models; defaults to :ThreadSpawn
+    # Allows options:
+    # :model:: :ConcurrModel - the concurrency model rainbows should use.
+    # :connections:: int - the number of worker connections to use.
+    # :timeout:: int - the keepalive timeout. zero disables keepalives.
+    def use_concurrency(options={})
+      @concurrency ||= {:model => :ThreadSpawn}
+      @concurrency.merge!(options)
+    end
+
   end
 
 end
