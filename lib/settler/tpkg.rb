7a5ff06a0f2ac95d37691cffa289afe1a9492408
jcastagna <jcastagna@fa7227d9-076c-0410-bae7-df9668f59714>
2010-02-26 19:43:06 +0000

    fixing tpkg to always use sudo so it doesn't break things when auto-sudoing
    git-svn-id: svn://subversion.flight.yellowpages.com/webtools/gems/sunshine/trunk@954 fa7227d9-076c-0410-bae7-df9668f59714

--- lib/settler/tpkg.rb
+++ lib/settler/tpkg.rb
@@ -2,12 +2,14 @@ class Settler
 
   class Tpkg < Dependency
 
+    self.sudo = true
+
     def initialize(dependency_lib, name, options={}, &block)
       super(dependency_lib, name, options) do
         pkg_name = @pkg.dup
         pkg_name << "-#{options[:version]}" if options[:version]
         pkg_name << "-#{options[:rel]}" if options[:rel]
-        pkg_name << (options[:arch] ? "-#{options[:arch]}" : "-$(uname -p)")
+        pkg_name << "-#{options[:arch]}" if options[:arch]
 
         install    "tpkg -n -i #{pkg_name}"
         uninstall  "tpkg -n -r #{pkg_name}"
